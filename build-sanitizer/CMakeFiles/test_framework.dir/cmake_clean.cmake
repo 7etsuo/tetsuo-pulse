14d98548e560a6d25dcb8c3ebd243e11c29ee89c
tetsuo <tetsuo.solana@gmail.com>
2025-11-27 22:02:49 -0700

    chore: update .gitignore to exclude build directories
    - Add build-*/ pattern for build-sanitizer, build-coverage, etc.
    - Add coverage file patterns (*.gcno, *.gcda, *.gcov, coverage.info)
    - Add benchmark_* executable pattern
    - Remove build-sanitizer/ from tracking

--- build-sanitizer/CMakeFiles/test_framework.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_framework.dir/src/test/Test.c.o"
-  "CMakeFiles/test_framework.dir/src/test/Test.c.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/test_framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
