6e9273b1a9fc04d699780e8cbe2a7ea21cd416a1
tetsuo <tetsuo.solana@gmail.com>
2025-11-27 22:02:49 -0700

    chore: update .gitignore to exclude build directories
    - Add build-*/ pattern for build-sanitizer, build-coverage, etc.
    - Add coverage file patterns (*.gcno, *.gcda, *.gcov, coverage.info)
    - Add benchmark_* executable pattern
    - Remove build-sanitizer/ from tracking

--- build-sanitizer/CMakeFiles/socket_shared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsocket.pdb"
-  "libsocket.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/socket_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
